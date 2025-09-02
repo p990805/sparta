b0269ae6a4bd4c12ab3b5ee29c91f6b9b4bd8986
박주찬 <p990805@naver.com>
2025-09-02 23:21:57 +0900

    Test : lombok, application.properties

---
+++ src/main/java/com/sparta/sparta/Memo.java
@@ -0,0 +1,14 @@
+package com.sparta.sparta;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    private String username;
+    private String contents;
+
+}
