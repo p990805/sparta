9a7be9028698dd18de08475f6ea7551dafaa9d22
박주찬 <p990805@naver.com>
2025-09-05 01:54:46 +0900

    Feat : 강한 결합을 느슨한 결합으로

--- 1week/memo/src/main/java/com/sparta/memo/service/MemoService.java
+++ 1week/memo/src/main/java/com/sparta/memo/service/MemoService.java
@@ -13,8 +13,8 @@ public class MemoService {
     private final MemoRepository memoRepository;
 
 
-    public MemoService(JdbcTemplate jdbcTemplate) {
-        this.memoRepository = new MemoRepository(jdbcTemplate);
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
