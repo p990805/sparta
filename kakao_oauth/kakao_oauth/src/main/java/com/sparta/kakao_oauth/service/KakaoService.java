4d64a0191f50f0641e4e80386955d8161e6429ab
박주찬 <p990805@naver.com>
2025-09-12 11:53:56 +0900

    init : 프로젝트 생성

---
+++ kakao_oauth/kakao_oauth/src/main/java/com/sparta/kakao_oauth/service/KakaoService.java
@@ -0,0 +1,30 @@
+package com.sparta.kakao_oauth.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.kakao_oauth.dto.KakaoUserInfoDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j(topic = "KAKAO Login")
+@Service
+@RequiredArgsConstructor
+public class KakaoService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
+    private final JwtUtil jwtUtil;
+
+    public String kakaoLogin(String code) throws JsonProcessingException {
+        // 1. "인가 코드"로 "액세스 토큰" 요청
+        String accessToken = getToken(code);
+
+        // 2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
+        KakaoUserInfoDto kakaoUserInfo = getKakaoUserInfo(accessToken);
+
+        return null;
+    }
+
---
+++ kakao_oauth/kakao_oauth/src/test/java/com/sparta/kakao_oauth/KakaoOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kakao_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
